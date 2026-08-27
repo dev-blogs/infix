70223016a5dffc86d12d5181d3d7c6b20a1b7143
zheka <zheka0783@yandex.ru>
2026-08-27 21:25:37 +0300

    Code refactoring. Refactoring accordting to chatGPT advice

--- infix/main.cpp
+++ infix/main.cpp
@@ -4,102 +4,94 @@
 
 bool isOperand(char ch);
 bool isOperator(char ch);
-bool priority_less_then(char opTop, char opThis);
+int precedence(char op);
+void processOperator(Stack& stack, std::string& output, char opThis);
 
 int main()
 {
-	Stack stack(10);
-	std::string expression = "2+3*4-5";
-	std::string output = "";
-	std::cout << expression << std::endl;
+    std::string expression = "2+3";
+    std::string output;
 
-	for (int i = 0; i < expression.size(); i++)
-	{
-		char element = expression[i];
-		if (isOperand(element))
-		{
-			output.push_back(element);
-		}
-		else if (stack.isEmpty())
-		{
-			stack.push(element);
-		}
-		else
-		{
-			if (element == '(')
-			{
-				stack.push(element);
-				continue;
-			}
+    Stack stack(expression.size());
 
-			if (element == ')')
-			{
-				char el = stack.pop();
-				while (el != '(' && !stack.isEmpty())
-				{
-					output.push_back(el);
-					el = stack.pop();
-				}
-				continue;
-			}
+    std::cout << expression << std::endl;
 
-			char opTop = stack.pop();
-			char opThis = element;
+    for (char element : expression)
+    {
+        if (isOperand(element))
+        {
+            output.push_back(element);
+        }
+        else if (element == '(')
+        {
+            stack.push(element);
+        }
+        else if (element == ')')
+        {
+            while (!stack.isEmpty())
+            {
+                char opTop = stack.pop();
+                if (opTop == '(') break;
+                output.push_back(opTop);
+            }
+        }
+        else if (isOperator(element))
+        {
+            processOperator(stack, output, element);
+        }
+    }
 
-			if (opTop == '(')
-			{
-				stack.push(opTop);
-				stack.push(element);
-				continue;
-			}
-			
-			if (!priority_less_then(opTop, opThis))
-			{
-				output.push_back(opTop);						
-				stack.push(opThis);
-			}
-			else
-			{
-				stack.push(opTop);
-				stack.push(opThis);
-			}				
-		}
-	}
+    while (!stack.isEmpty())
+    {
+        output.push_back(stack.pop());
+    }
 
-	while (!stack.isEmpty())
-	{
-		output.push_back(stack.pop());
-	}
+    std::cout << "output: " << output << std::endl;
 
-	std::cout << "output: " << output << std::endl;
-
-	return 0;
+    return 0;
 }
 
 bool isOperand(char ch)
 {
-	if (ch - '0' >= 0 && ch - '0' <= 9)
-	{
-		return true;
-	}
-	return false;
+	return ch >= '0' && ch <= '9';
 }
 
 bool isOperator(char ch)
 {
-	if (ch == '+' || ch == '-' || ch == '*' || ch == '/')
-	{
-		return true;
-	}
-	return false;
+	return ch == '+' || ch == '-' || ch == '*' || ch == '/';
 }
 
-bool priority_less_then(char opTop, char opThis)
+int precedence(char op)
 {
-	if ((opTop == '+' || opTop == '-') 
-		&& (opThis == '*' || opThis == '/'))
+	if (op == '+' || op == '-')
+		return 1;
+
+	if (op == '*' || op == '/')
+		return 2;
+
+	return 0;
+}
+
+void processOperator(Stack& stack, std::string& output, char opThis)
+{
+	while (!stack.isEmpty())
 	{
-		return true;
+		char opTop = stack.pop();
+
+		if (opTop == '(')
+		{
+			stack.push(opTop);
+			break;
+		}
+
+		if (precedence(opTop) < precedence(opThis))
+		{
+			stack.push(opTop);
+			break;
+		}
+
+		output.push_back(opTop);
 	}
-	return false;
+
+	stack.push(opThis);
 }
